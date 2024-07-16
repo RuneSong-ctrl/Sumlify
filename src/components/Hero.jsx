7ec8818bbbc97945f9acd591b930bb6ce923cbfd
Rama Devan <148033431+RuneSong-ctrl@users.noreply.github.com>
2024-07-16 19:50:16 +0800

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 
-import { logo } from "../assets";
-
 const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
       <nav className="flex justify-between items-center w-full mb-10 pt-3">
-        <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
-
+       <span className="text-2xl font-bold">Sumlify</span>
         <button
           type="button"
           onClick={() =>
